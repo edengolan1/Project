e3927b6ad414d292a52fca173cbbc55b8377c714
Eden Golan <eden9123@gmail.com>
2024-10-19 19:28:34 +0300

    added video

--- src/components/Video.js
+++ src/components/Video.js
@@ -27,7 +27,7 @@ function Video() {
           zIndex: -1,
         }}
       >
-        <source src="/assests/Video2.mp4" type="video/mp4" />
+        <source src="/assets/Video2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <Box
